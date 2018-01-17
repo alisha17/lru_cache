322319aa4f34ac470998abcd7a7e07233bf19676
alisha17 <anejaalisha@yahoo.com>
2018-01-17 16:33:13 +0900

    Raw pointer for List

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,9 @@ use std::fmt::Debug;
 fn main(){
 }
 
-type Link<T> = Option<Box<Node<T>>>;
-
 #[derive(Debug)]
 pub struct List<T> {
-    head: Link<T>,
+    head: *mut Node<T>,
     tail: *mut Node<T>,
 }
 
@@ -16,76 +14,60 @@ pub struct List<T> {
 struct Node<T> {
     prev: *mut Node<T>,
     elem: T,
-    next: Link<T>,
+    next: *mut Node<T>,
 }
 
 impl<T> List<T> where T: Debug+PartialEq {
     pub fn new() -> Self {
-        List { head: None, tail: ptr::null_mut() }
+        List { head: ptr::null_mut(), tail: ptr::null_mut() }
     }
 
     pub fn push(&mut self, elem: T) {
-        let mut new_tail = Box::new(Node {
+        let mut new_tail_box = Box::new(Node {
             prev: ptr::null_mut(),
             elem: elem,
-            next: None,
+            next: ptr::null_mut(),
         });
 
-        let raw_tail: *mut _ = &mut *new_tail;
+        let new_tail = Box::into_raw(new_tail_box);
 
         if !self.tail.is_null() {
             unsafe {
-                new_tail.prev = self.tail;
-                (*self.tail).next = Some(new_tail);
+                (*new_tail).prev = self.tail;
+                (*self.tail).next = new_tail;
             }
         } else {
-            self.head = Some(new_tail);
+            (*self.head) = new_tail;
         }
 
-        self.tail = raw_tail;
+        (*self.tail) = new_tail;
     }
 
-    pub fn pop(&mut self) -> Option<T> {
-        self.head.take().map(|head| {
-            let head = *head;
-            self.head = head.next;
-
-            match self.head.as_mut() {
-                Some(x) => x.prev = ptr::null_mut(),
-                None => {},
-            }
+    pub fn pop(&mut self) -> T {
+            (*self.head).prev = ptr::null_mut();
             
-            if self.head.is_none() {
+            if self.head.is_null() {
                 self.tail = ptr::null_mut();
             }
-
-            head.elem
-
-        })
+            (*self.head).elem
     }
 
     pub fn cut(&mut self, elem:T) {
         let mut current = self.head;
         let mut last_elem = self.tail;
 
-        while current.is_some() {
-            match current.as_ref() {
-                Some(x) => {
-                    if x.elem == elem {
-                        (x.prev).next = x.next;
-                        (x.next).prev = x.prev;
-                        x.next = None;
-                        x.prev = self.tail;
-                    }
-                }
-
-                None => {},
-                    
-                }
+        while !current.is_null() {
+            if (*current).elem == elem {
+                unsafe {
+                   (*(*current).prev).next = (*current).next;
+                   (*(*current).next).prev = (*current).prev;
+                    (*current).next = ptr::null_mut();
+                    (*current).prev = self.tail
+                } 
             }
         }
     }
-
+}
 
 #[cfg(test)]
 mod tests {
@@ -97,19 +79,14 @@ mod tests {
 
        let mut list = List::<u32>::new();
 
-        // Check empty list behaves right
-        assert_eq!(list.pop(), None);
-
         list.push(1);
         list.push(2);
         list.push(3);
 
         // Check normal removal
-        assert_eq!(list.pop(), Some(1));
-        assert_eq!(list.pop(), Some(2));
-        assert_eq!(list.pop(), Some(3));        
-        assert_eq!(list.pop(), None);
-
+        assert_eq!(list.pop(), 1);
+        assert_eq!(list.pop(), 2);
+        assert_eq!(list.pop(), 3);        
     }
 }
 
